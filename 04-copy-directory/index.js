d08d9845722a710c6f92e8e4777fc1fd18849a78
zharskaya97 <aleksei.zharskii@gmail.com>
2021-11-07 19:13:17 +0400

    feat: add directory copy

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,16 @@
+let fs = require('fs');
+let path = require('path');
+const {stdout} = process;
+fs.mkdir('./04-copy-directory/files-copy', { recursive: true }, (err) => {
+    if (err) throw err;
+  });
 
+fs.readdir('./04-copy-directory/files', {withFileTypes: true}, (err, files) => {
+    if (err) throw err;
+    stdout.write('Копия создана');
+    for (let i = 0; i < files.length;i++) {
+    fs.copyFile(`./04-copy-directory/files/${path.basename(files[i].name)}`, `./04-copy-directory/files-copy/${path.basename(files[i].name)}`, err => {
+        if(err) throw err;
+        });
+    }
+})
